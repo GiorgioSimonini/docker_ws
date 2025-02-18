e456571de2154106c6e7a5cb1132850ca80675ee
GiorgioSimonini <giorgio.simonini.gs@gmail.com>
2025-02-18 23:11:04 +0100

    working checkpoint

--- first_compile.bash
+++ first_compile.bash
@@ -1,1 +1,3 @@
-catkin build -DCMAKE_BUILD_TYPE=Release -DFranka_DIR:PATH=/libfranka/build franka_ros
+vcs import src/franka_ros2 < src/franka_ros2/franka.repos --recursive --skip-existing
+# colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release franka_ros2
+colcon build
