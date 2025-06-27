f17b9e93013c5d0b83ab2763766b935ef488637c
GiorgioSimonini <giorgio.simonini.gs@gmail.com>
2025-06-27 14:22:03 +0200

    checkpoint, use old run script

--- first_compile.bash
+++ first_compile.bash
@@ -1,7 +1,7 @@
 vcs import src/franka_ros2 < src/franka_ros2/franka.repos --recursive --skip-existing
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-select libfranka
 source install/setup.bash
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-select franka_bringup franka_description franka_example_controllers franka_fr3_moveit_config franka_gazebo_bringup franka_ign_ros2_control franka_gripper franka_hardware franka_msgs
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release --packages-select franka_semantic_components franka_robot_state_broadcaster franka_bringup franka_description franka_example_controllers franka_fr3_moveit_config franka_gazebo_bringup franka_ign_ros2_control franka_gripper franka_hardware franka_msgs
 source install/setup.bash
 colcon build --symlink-install
 
