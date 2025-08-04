b535d334acb6e7b77cd83232392d136deeef515c
GiorgioSimonini <giorgio.simonini.gs@gmail.com>
2025-08-04 16:03:03 +0200

    no runArgs devcontainer, recreate env in build.bash

--- build.bash
+++ build.bash
@@ -2,5 +2,7 @@
 
 export MYUID=$(id -u)
 export MYGID=$(id -g)
+echo "MYUID=$(id -u)" > .devcontainer/.env
+echo "MYGID=$(id -g)" >> .devcontainer/.env
 
 docker compose -f .devcontainer/docker-compose.yml build
